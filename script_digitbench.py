6d5eec125632d00ee40a47d733e98ebbe4878553
Peronne Hopstaken <p.j.m.hopstaken@student.tue.nl>
2023-10-02 15:55:17 +0200

    With working stiffness matrix

--- script_digitbench.py
+++ script_digitbench.py
@@ -3,7 +3,7 @@ import numpy as np
 import scipy as sp
 import matplotlib.pyplot as plt
 
-# function for conversion of dense to sparse matrix
+# Function for conversion of dense to sparse matrix
 def dense2sparse(A):
 
     row_ind,col_ind = np.nonzero(A)
@@ -22,8 +22,8 @@ class beam2D_v2:
         self.node_ID = node[:,0].astype(int)
         self.node_coord = node[:,1:3]
         self.dof = np.array([[self.node_ID[0],1],   #dof 1 = axial load
-                             [self.node_ID[0],2],   #dof 2 = bending load
-                             [self.node_ID[0],3],   #dof 3 = moment load
+                             [self.node_ID[0],2],   #dof 2 = bending 
+                             [self.node_ID[0],3],   #dof 3 = moment 
                              [self.node_ID[1],1],
                              [self.node_ID[1],2],
                              [self.node_ID[1],3]],dtype="int")
@@ -37,7 +37,7 @@ class beam2D_v2:
         self.T = np.concatenate((self.s,self.t),axis=0)
         self.R = sp.linalg.block_diag(self.T,one,self.T,one)
 
-        # compute the stiffness matrix
+        # Compute the stiffness matrix
         self.compute_K()
 
     def compute_K(self):
@@ -49,17 +49,11 @@ class beam2D_v2:
                                                     [0, -self.E*self.I/self.L**3*12, -self.E*self.I/self.L**2*6, 0, self.E*self.I/self.L**3*12, -self.E*self.I/self.L**2*6],
                                                     [0, self.E*self.I/self.L**2*6,   self.E*self.I/self.L*2,     0, -self.E*self.I/self.L**3*6, self.E*self.I/self.L*4    ]],dtype="float")
 
-        # Global stiffness matrix
-        self.Ke = dense2sparse(np.linalg.multi_dot([np.transpose(self.R),self.Kl,self.R]))       #per element
-
-        # dense to sparse conversion
-        # row_ind,col_ind = np.nonzero(self.Kg)
-        # data = self.Kg[row_ind,col_ind]
-        # self.Kg = sp.sparse.csr_matrix((data,(row_ind,col_ind)),shape=self.Kg.shape)
+        # Global stiffness matrix per element
+        self.Ke = dense2sparse(np.linalg.multi_dot([np.transpose(self.R),self.Kl,self.R]))
 
 
     def compute_Z(self,model_dofs):
-        #put a 1 where self.dof == model_dofs
 
         self.row_index = []
         self.col_index = []
@@ -95,16 +89,16 @@ params = {
 # Node geometry
 coord = np.zeros((11,3))
 coord[0,:] = [0, 0.0, 0.0]
-coord[1,:] = [1, 0.0, 996.0]
-coord[2,:] = [2, 0.0, Lh]
+coord[1,:] = [1, 917.0, 0.0]
+coord[2,:] = [2, Lv, 0.0]
 coord[3,:] = [3, Lv, Lh-330]
 coord[4,:] = [4, Lv, Lh]
-coord[5,:] = [5, Lv, 0.0]
-coord[6,:] = [6, 917.0, 0.0]
-coord[7,:] = [7, 917.0, 675.0]
-coord[8,:] = [8, 917.0, 996.0-21.0]
-coord[9,:] = [9, 917.0, 996.0]
-coord[10,:] = [10, 660.0, 996.0]
+coord[5,:] = [5, 0.0, Lh]
+coord[6,:] = [6, 0.0, 996.0]
+coord[7,:] = [7, 660.0, 996.0]
+coord[8,:] = [8, 917.0, 996.0]
+coord[9,:] = [9, 917, 996.0-21.0]
+coord[10,:] = [10, 917.0, 675.0]
 
 # Elements
 eTop = np.zeros((12,2),dtype=int)
@@ -116,20 +110,21 @@ eTop[10,1] = coord[1,0]
 eTop[11,0] = coord[0,0]
 eTop[11,1] = coord[6,0]
 
+# Plot the initial geometry
+plt.plot([coord[eTop[:,0],1],coord[eTop[:,1],1]],[coord[eTop[:,0],2],coord[eTop[:,1],2]])
+
 # Arrays
 myElements = []
-
-#%% prototype of the model class
-
-
+K = []
 model_dofs = np.zeros((0,2),dtype=int)
 
-## Main code
+#%% Prototype of the model class
+
 # Get unique dof list
 for i in range(0,eTop.shape[0]):
 
     myElements.append(beam2D_v2(params,coord[eTop[i,:],:])) 
-    model_dofs = np.concatenate((model_dofs,myElements[i].dof),axis=0)      #All dofs
+    model_dofs = np.concatenate((model_dofs,myElements[i].dof),axis=0)      #List of all dofs
 
 model_dofs = np.unique(model_dofs,axis=0)                                   #Remove double dofs
 
@@ -138,22 +133,11 @@ for i in range(0,eTop.shape[0]):
 
     myElements[i].compute_Z(model_dofs)
 
-#%%
+    Ze_sp = myElements[i].Ze
+    Ke_sp = myElements[i].Ke
 
-Ze_sp = myElements[0].Ze
-Ke_sp = myElements[0].Ke
+    K += Ze_sp.transpose() @ Ke_sp @ Ze_sp                  #Global stiffness matrix
 
-#print(Ke)
-#print(Ke_sp.toarray())
-
-K = Ze_sp.transpose() @ Ke_sp @ Ze_sp
-
-#%%
-
-for myElement in myElements:
-    K = Ze_sp.transpose() @ Ke_sp @ Ze_sp
-
-#%%
 
 #%%
 
@@ -182,35 +166,48 @@ BC[2*Ndof+1,0] = 0  #y-translation node 2
 #                       [self.dof[5,0],self.dof[5,1],0,0]])
 
 
-## Main code
-Kg = np.zeros(((len(coord)+1)*Ndof+2,(len(coord)+1)*Ndof+2)).astype(object)
-Ze = np.zeros((6,(len(coord)+1)*Ndof+2)).astype(object)
-force = np.zeros(((len(coord)+1)*Ndof+2,1)).astype(object)
-
-for i in range(len(eTop)-1):
-    id0 = eTop[i,0]
-    id1 = eTop[i,1]
-    node = np.zeros((2,3)).astype(object)
-    for j in range(len(coord)):
-        if coord[j,0] == id0:
-            node[0,:] = coord[j,:]
-        if coord[j,0] == id1:
-            node[1,:] = coord[j,:]
-
-    element = beam2D_v2(params, node)
-    element.__init__(params,node)
-    Keg = element.stiffness_matrix()
-
-    Ze[0:6,3*i:3*i+6] = np.ones((6,6))
-    Kg += np.linalg.multi_dot([np.transpose(Ze),Keg,Ze])    #Global stiffness matrix
-    force_e = load[Ndof*i:Ndof*i+6]
-    force += np.dot(np.transpose(Ze),force_e)
 
 
 
+# %% Validating the dense2sparse function 
 
-#u = np.linalg.lstsq(Kg, force)
+test_matrix1 = np.zeros((6,6))
+test_matrix1[0,0] = 2
+test_matrix1[0,3] = 1
+test_matrix1[1,2] = 1
+test_matrix1[2,3] = 2
+test_matrix1[3,0] = 1
+test_matrix1[3,4] = 3
+test_matrix1[4,1] = 2
+test_matrix1[4,5] = 2
+test_matrix1[5,2] = 1
+test_matrix1[5,4] = 1
 
+test_matrix2 = np.zeros((6,6))
+test_matrix2[0,0] = 2
+test_matrix2[0,4] = 1
+test_matrix2[1,3] = 1
+test_matrix2[2,5] = 2
+test_matrix2[3,1] = 1
+test_matrix2[3,3] = 3
+test_matrix2[4,0] = 2
+test_matrix2[4,4] = 2
+test_matrix2[5,2] = 1
+test_matrix2[5,5] = 1
 
+print(test_matrix1)
+print('=====')
+print(test_matrix2)
 
-# %%
+test_sparse1 = dense2sparse(test_matrix1)
+tets_sparse2 = dense2sparse(test_matrix2)
+
+test_trans = test_sparse1.transpose()
+
+test = test_sparse1 + tets_sparse2
+
+print('=============================================')
+print(test)
+print('-----')
+print(test_trans)
+
